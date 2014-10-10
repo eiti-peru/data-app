94683e55356549ba6060e942cfe9675ffed4c22c
Christian Kreutz <ck@crisscrossed.de>
2014-10-10 13:35:14 +0200

    remove obsolete code

--- js/colored_map.js
+++ js/colored_map.js
@@ -1,52 +1,4 @@
 $(function (){
-  // var colors = [
-  //   '#B10026', '#E31A1C', '#FC4E2A', '#FD8D3C', '#FEB24C', '#FED976',
-  //   '#FFFFB2'
-  // ]
-
-  // var fillCss = function(condition, color, table){
-  //   table || (table = 'eiti_peru')
-  //   return "#" + table + " [ " + condition + "] {"+
-  //     "polygon-fill: " + color + ";" +
-  //     "polygon-opacity: 0.7;" +
-  //     "line-color: #FFF;" +
-  //     "line-width: 1;" +
-  //     "line-opacity: 1;" +
-  //     "}";
-  // }
-
-  // var cssFor = function(column, callback, account){
-  //   account || (account = 'crisscrossed');
-  //   var cacheKey = 'cssFor' + column + new Date().toDateString();
-  //   cacheKey = cacheKey.replace(/\s/g, '_');
-
-  //   var q = "SELECT cartodb_id, the_geom, the_geom_webmercator, departamento, year, canon_minero_gobiernos_locales_del_departamento, canon_minero_gobiernos_regionales, canon_gasifero_universidades_nacionales, canon_gasifero_gobiernos_regionales, canon_gasifero_gobiernos_locales_del_departamento, canon_y_sobrecanon_petrolero_universidades_nacionales, canon_y_sobrecanon_petrolero_institutos, canon_y_sobrecanon_petrolero_gobiernos_regionales,regalias_mineras_universidades_naccionales, regalias_mineras_gobiernos_regionales, regalias_mineras_gobiernos_locales_del_departamenteo, derechos_de_vigencia, total::integer as total FROM eiti_peru where year = '" + column + "'";
-  //   value = 'total'
-  //   if(cache = JSON.parse(sessionStorage[cacheKey] || null))
-  //     callback(cssFromRows(cache, value));
-  //   else{
-  //     $.get('http://' + account + '.cartodb.com/api/v2/sql?q=' + q ).done(function(response){
-  //       sessionStorage[cacheKey] = JSON.stringify(response.rows);
-  //       callback(cssFromRows(response.rows, value));
-  //     });
-  //   }
-  // };
-
-  // var cssFromRows = function(rows, column){
-
-  //   var min   = _(rows).min(function(row){ return parseInt(row[column]) });
-  //   min = parseInt(min[column]);
-
-  //   var max   = _(rows).max(function(row){ return parseInt(row[column]) });
-  //   max = parseInt(max[column]);
-  //   var step  = (max - min) / colors.length;
-  //   var css = _(colors).map(function(color, index){
-  //     var limit  = (max + 1) - step * index;
-  //     var subCss = fillCss(column + ' <= ' + limit, color);
-  //     return subCss;
-  //   }).join('');
-  //   return css;
-  // };
 
   function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
@@ -140,7 +92,6 @@ $(function (){
       hideTooltip();
     });
     yearHandler(subLayer);
-    // setCss($('li.selected'), subLayer);
   })
   .error(function(err) {
     console.log(err);
